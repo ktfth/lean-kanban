3d506d0ddf20d080429f5df09632fa8f75526a78
Kaique Silva <kaeyosthaeron@gmail.com>
2020-02-05 20:04:54 -0300

    Integrated with backend

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
+import io from 'socket.io-client';
+import feathers from '@feathersjs/client';
+
+// Socket.io is exposed as the `io` global.
+const socket = io('http://localhost:3030');
+// @feathersjs/client is exposed as the `feathers` global.
+const app = feathers();
+
+app.configure(feathers.socketio(socket));
+app.configure(feathers.authentication());
+
+app.service('v1/cards').create({
+  text: 'Next connection'
+});
+
 export default function Index() {
   return (
     <div>
